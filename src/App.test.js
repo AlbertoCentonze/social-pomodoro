36c0c803d821be0c197df1848697524d0d1702a6
Alberto <albicento.ac@gmail.com>
2020-05-15 12:11:45 +0200

    Fixed vertical scrolling on smaller screen. Started working on tests to be sure that the timer works

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,25 @@
-// TODO assicurarsi che il timer non si disconnetta tramite test di 25 min
+import { socket } from "./services/socket";
+
+test("socket is online", () => {
+  let connected = false;
+  socket.on("connection", (data) => {
+    console.log(data);
+    connected = data;
+  });
+  expect(connected).toBe(true);
+});
+
+test("timer won't get stuck for 25min", () => {
+  socket.emit("newTimer", "test1");
+  let currentDuration = 1495;
+  let stucked = false;
+  setInterval(() => {
+    currentDuration -= 1;
+  });
+  socket.on("test1", (data) => {
+    while (currentDuration > 1300) {
+      stucked = currentDuration < data.duration;
+    }
+  });
+  expect(stucked).toBe(false);
+});
